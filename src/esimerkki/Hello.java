728f25a254cb1207021de26de5471ef6e9f1da33
mika <koneautomaatio@hotmail.com>
2017-10-28 15:55:21 +0300

    ei muutoksia, kait

---
+++ src/esimerkki/Hello.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esimerkki;
+
+/**
+ *
+ * @author s1600888
+ */
+public class Hello {
+    public static void main(String [] args) 
+  {
+    System.out.println("Hello World!");
+  }
+    
+}
